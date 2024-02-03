83a60f9b76d73eff1799e31cc026aea01984043c
Zahidul-Turja <zahidulturja@gmail.com>
2024-02-04 00:16:46 +0600

    Faq style

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -1,6 +1,10 @@
+import styled from "styled-components";
+
+import { About } from "../styles";
+
 function FaqSection() {
   return (
-    <div className="faq">
+    <Faq>
       <h2>
         Any Questions? <span>FAQ</span>
       </h2>
@@ -13,6 +17,7 @@ function FaqSection() {
             quasi blanditiis repellat.
           </p>
         </div>
+        <div className="faq-line"></div>
       </div>
       <div className="question">
         <h4>Daily Schedual.</h4>
@@ -23,6 +28,7 @@ function FaqSection() {
             quasi blanditiis repellat.
           </p>
         </div>
+        <div className="faq-line"></div>
       </div>
       <div className="question">
         <h4>Different Payment Methods.</h4>
@@ -33,6 +39,7 @@ function FaqSection() {
             quasi blanditiis repellat.
           </p>
         </div>
+        <div className="faq-line"></div>
       </div>
       <div className="question">
         <h4>What Products Do You Offer?</h4>
@@ -43,9 +50,41 @@ function FaqSection() {
             quasi blanditiis repellat.
           </p>
         </div>
+        <div className="faq-line"></div>
       </div>
-    </div>
+    </Faq>
   );
 }
+
+const Faq = styled(About)`
+  display: block;
+  span {
+    display: block;
+  }
+  h2 {
+    padding-bottom: 2rem;
+    font-weight: lighter;
+  }
+
+  .faq-line {
+    background: #ccc;
+    height: 0.2rem;
+    margin: 2rem 0rem;
+    width: 100%;
+  }
+
+  .question {
+    padding: 3rem 0rem;
+    cursor: pointer;
+  }
+
+  .answer {
+    padding: 2rem 0rem;
+
+    p {
+      padding: 1rem 0rem;
+    }
+  }
+`;
 
 export default FaqSection;
