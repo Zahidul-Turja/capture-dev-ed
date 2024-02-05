b0652c4d7f05563484b1d981dc4a1f7681b9cf9a
Zahidul-Turja <zahidulturja@gmail.com>
2024-02-05 15:28:08 +0600

    Nav added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import React from "react";
 // ! Global Style
 import GlobalStyle from "./components/GlobalStyle";
 
+// ! Components
+import Nav from "./components/Nav";
+
 // ! Pages
 import AboutUs from "./pages/AboutUs";
 
@@ -10,6 +13,7 @@ function App() {
   return (
     <div>
       <GlobalStyle />
+      <Nav />
       <AboutUs />
     </div>
   );
